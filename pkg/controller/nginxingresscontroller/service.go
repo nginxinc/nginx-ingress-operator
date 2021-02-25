nginxinc/nginx-ingress-operator

commit 8238e1099fb78e316e133b4e69fce14117294cf2

Add IngressLink support

pkg/controller/nginxingresscontroller/service.go
@@ -12,7 +12,7 @@ func serviceForNginxIngressController(instance *k8sv1alpha1.NginxIngressControll
 		ObjectMeta: v1.ObjectMeta{
 			Name:      instance.Name,
 			Namespace: instance.Namespace,
-			Labels:    instance.Spec.ExtraLabels,
+			Labels:    instance.Spec.Service.ExtraLabels,
 		},
 		Spec: corev1.ServiceSpec{
 			Ports: []corev1.ServicePort{

deploy/crds/k8s.nginx.org_nginxingresscontrollers_crd.yaml
@@ -84,11 +84,6 @@ spec:
               description: Enable TLS Passthrough on port 443. Requires enableCRDs
                 set to true.
               type: boolean
-            extraLabels:
-              additionalProperties:
-                type: string
-              description: Specifies extra labels of the service.
-              type: object
             globalConfiguration:
               description: The GlobalConfiguration resource for global configuration
                 of the Ingress Controller. Format is namespace/name. Requires enableCRDs
@@ -212,11 +207,27 @@ spec:
                   description: 'Specifies the name of the service with the type LoadBalancer
                     through which the Ingress controller pods are exposed externally.
                     The external address of the service is used when reporting the
-                    status of Ingress resources. Note: Only if ServiceType is different
-                    than LoadBalancer.'
+                    status of Ingress resources. Note: Only if serviceType is NodePort.'
+                  type: string
+                ingressLink:
+                  description: 'Specifies the name of the IngressLink resource, which
+                    exposes the Ingress Controller pods via a BIG-IP system. The IP
+                    of the BIG-IP system is used when reporting the status of Ingress,
+                    VirtualServer and VirtualServerRoute resources. Requires reportIngressStatus.enable
+                    set to true. Note: Only if serviceType is NodePort and reportIngressStatus.externalService
+                    is not set.'
                   type: string
               required:
               - enable
+              type: object
+            service:
+              description: The service of the Ingress controller.
+              properties:
+                extraLabels:
+                  additionalProperties:
+                    type: string
+                  description: Specifies extra labels of the service.
+                  type: object
               type: object
             serviceType:
               description: 'The type of the Service for the Ingress Controller. Valid

pkg/apis/k8s/v1alpha1/nginxingresscontroller_types.go
@@ -45,7 +45,6 @@ type NginxIngressControllerSpec struct {
 	// +kubebuilder:validation:Optional
 	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	EnablePreviewPolicies bool `json:"enablePreviewPolicies"`
-	// +kubebuilder:validation:Optional
 	// A class of the Ingress controller. The Ingress controller only processes Ingress resources that belong to its
 	// class (in other words, have the annotation “kubernetes.io/ingress.class”).
 	// Additionally, the Ingress controller processes Ingress resources that do not have that annotation,
@@ -53,10 +52,10 @@ type NginxIngressControllerSpec struct {
 	// +kubebuilder:validation:Optional
 	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	IngressClass string `json:"ingressClass"`
-	// Specifies extra labels of the service.
+	// The service of the Ingress controller.
 	// +kubebuilder:validation:Optional
 	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
-	ExtraLabels map[string]string `json:"extraLabels,omitempty"`
+	Service *Service `json:"service"`
 	// Ignore Ingress resources without the “kubernetes.io/ingress.class” annotation.
 	// +kubebuilder:validation:Optional
 	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
@@ -189,9 +188,16 @@ type ReportIngressStatus struct {
 	Enable bool `json:"enable"`
 	// Specifies the name of the service with the type LoadBalancer through which the Ingress controller pods are exposed externally.
 	// The external address of the service is used when reporting the status of Ingress resources.
-	// Note: Only if ServiceType is different than LoadBalancer.
+	// Note: Only if serviceType is NodePort.
 	// +kubebuilder:validation:Optional
 	ExternalService string `json:"externalService"`
+	// Specifies the name of the IngressLink resource, which exposes the Ingress Controller pods via a BIG-IP system.
+	// The IP of the BIG-IP system is used when reporting the status of Ingress, VirtualServer and VirtualServerRoute resources.
+	// Requires reportIngressStatus.enable set to true.
+	// Note: Only if serviceType is NodePort and reportIngressStatus.externalService is not set.
+	// +kubebuilder:validation:Optional
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
+	IngressLink string `json:"ingressLink,omitempty"`
 }
 
 // Prometheus defines the Prometheus metrics for the Ingress Controller.
@@ -207,10 +213,17 @@ type Prometheus struct {
 	Port *uint16 `json:"port"`
 }
 
-// App Protect support configuration.
+// AppProtect support configuration.
 type AppProtect struct {
 	// Enable App Protect.
 	Enable bool `json:"enable"`
+}
+
+// Service defines the Service for the Ingress Controller.
+type Service struct {
+	// Specifies extra labels of the service.
+	// +kubebuilder:validation:Optional
+	ExtraLabels map[string]string `json:"extraLabels,omitempty"`
 }
 
 // NginxIngressControllerStatus defines the observed state of NginxIngressController

pkg/apis/k8s/v1alpha1/zz_generated.deepcopy.go
@@ -126,12 +126,10 @@ func (in *NginxIngressControllerSpec) DeepCopyInto(out *NginxIngressControllerSp
 		*out = new(int32)
 		**out = **in
 	}
-	if in.ExtraLabels != nil {
-		in, out := &in.ExtraLabels, &out.ExtraLabels
-		*out = make(map[string]string, len(*in))
-		for key, val := range *in {
-			(*out)[key] = val
-		}
+	if in.Service != nil {
+		in, out := &in.Service, &out.Service
+		*out = new(Service)
+		(*in).DeepCopyInto(*out)
 	}
 	if in.HealthStatus != nil {
 		in, out := &in.HealthStatus, &out.HealthStatus
@@ -248,6 +246,29 @@ func (in *ReportIngressStatus) DeepCopy() *ReportIngressStatus {
 		return nil
 	}
 	out := new(ReportIngressStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Service) DeepCopyInto(out *Service) {
+	*out = *in
+	if in.ExtraLabels != nil {
+		in, out := &in.ExtraLabels, &out.ExtraLabels
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Service.
+func (in *Service) DeepCopy() *Service {
+	if in == nil {
+		return nil
+	}
+	out := new(Service)
 	in.DeepCopyInto(out)
 	return out
 }

pkg/controller/nginxingresscontroller/service_test.go
@@ -24,7 +24,9 @@ func TestServiceForNginxIngressController(t *testing.T) {
 		},
 		Spec: k8sv1alpha1.NginxIngressControllerSpec{
 			ServiceType: serviceType,
-			ExtraLabels: extraLabels,
+			Service: &k8sv1alpha1.Service{
+				ExtraLabels: extraLabels,
+			},
 		},
 	}
 	expected := &corev1.Service{

pkg/controller/nginxingresscontroller/utils.go
@@ -90,6 +90,8 @@ func generatePodArgs(instance *k8sv1alpha1.NginxIngressController) []string {
 			args = append(args, fmt.Sprintf("-external-service=%v", instance.Spec.ReportIngressStatus.ExternalService))
 		} else if instance.Spec.ServiceType == "LoadBalancer" {
 			args = append(args, fmt.Sprintf("-external-service=%v", instance.Name))
+		} else if instance.Spec.ReportIngressStatus.IngressLink != "" {
+			args = append(args, fmt.Sprintf("-ingresslink=%v", instance.Spec.ReportIngressStatus.IngressLink))
 		}
 	}
 

pkg/controller/nginxingresscontroller/utils_test.go
@@ -111,9 +111,35 @@ func TestGeneratePodArgs(t *testing.T) {
 					Namespace: namespace,
 				},
 				Spec: k8sv1alpha1.NginxIngressControllerSpec{
-					DefaultSecret:       "my-nginx-ingress/my-secret",
-					ServiceType:         "LoadBalancer",
-					ReportIngressStatus: &k8sv1alpha1.ReportIngressStatus{Enable: true},
+					DefaultSecret: "my-nginx-ingress/my-secret",
+					ServiceType:   "NodePort",
+					ReportIngressStatus: &k8sv1alpha1.ReportIngressStatus{
+						Enable:      true,
+						IngressLink: "my-ingresslink",
+					},
+				},
+			},
+			expected: []string{
+				"-nginx-configmaps=my-nginx-ingress/my-nginx-ingress",
+				"-default-server-tls-secret=my-nginx-ingress/my-secret",
+				"-enable-custom-resources=false",
+				"-report-ingress-status",
+				"-ingresslink=my-ingresslink",
+			},
+		},
+		{
+			instance: &k8sv1alpha1.NginxIngressController{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Spec: k8sv1alpha1.NginxIngressControllerSpec{
+					DefaultSecret: "my-nginx-ingress/my-secret",
+					ServiceType:   "LoadBalancer",
+					ReportIngressStatus: &k8sv1alpha1.ReportIngressStatus{
+						Enable:      true,
+						IngressLink: "my-invalid-ingresslink",
+					},
 				},
 			},
 			expected: []string{
@@ -173,6 +199,7 @@ func TestGeneratePodArgs(t *testing.T) {
 					ReportIngressStatus: &k8sv1alpha1.ReportIngressStatus{
 						Enable:          true,
 						ExternalService: "external",
+						IngressLink:     "my-invalid-ingressLink",
 					},
 					EnableLeaderElection: true,
 					WildcardTLS:          "my-nginx-ingress/wildcard-secret",

commit ad81ccf7ba5816da4e384e58ef9fba4c2ea9f6df

Fix ExtraLabels to be an optional field
* Bug introduced in: https://github.com/nginxinc/nginx-ingress-operator/commit/8238e1099fb78e316e133b4e69fce14117294cf2

pkg/controller/nginxingresscontroller/service.go
@@ -8,11 +8,16 @@ import (
 )
 
 func serviceForNginxIngressController(instance *k8sv1alpha1.NginxIngressController) *corev1.Service {
+	extraLabels := map[string]string{}
+	if instance.Spec.Service != nil {
+		extraLabels = instance.Spec.Service.ExtraLabels
+	}
+
 	return &corev1.Service{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      instance.Name,
 			Namespace: instance.Namespace,
-			Labels:    instance.Spec.Service.ExtraLabels,
+			Labels:    extraLabels,
 		},
 		Spec: corev1.ServiceSpec{
 			Ports: []corev1.ServicePort{