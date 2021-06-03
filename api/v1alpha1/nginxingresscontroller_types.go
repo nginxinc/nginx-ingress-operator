545f337f97ed0f866aad17da59b87345775d4f63
Luca Comellini <luca.com@gmail.com>
2021-06-02 19:43:13 -0700

    Add API Markers

--- api/v1alpha1/nginxingresscontroller_types.go
+++ api/v1alpha1/nginxingresscontroller_types.go
@@ -21,82 +21,102 @@ import (
 )
 
 // NginxIngressControllerSpec defines the desired state of NginxIngressController
+// +operator-sdk:csv:customresourcedefinitions:resources={Deployment,v1,nginx-ingress-operator}
 type NginxIngressControllerSpec struct {
 	// The type of the Ingress Controller installation - deployment or daemonset.
 	// +kubebuilder:validation:Enum=deployment;daemonset
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	Type string `json:"type"`
 	// Deploys the Ingress Controller for NGINX Plus. The default is false meaning the Ingress Controller will be deployed for NGINX OSS.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	NginxPlus bool `json:"nginxPlus"`
 	// The image of the Ingress Controller.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	Image Image `json:"image"`
 	// The number of replicas of the Ingress Controller pod. The default is 1. Only applies if the type is set to deployment.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	Replicas *int32 `json:"replicas"`
 	// The TLS Secret for TLS termination of the default server. The format is namespace/name.
 	// The secret must be of the type kubernetes.io/tls.
 	// If not specified, the operator will generate and deploy a TLS Secret with a self-signed certificate and key.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	DefaultSecret string `json:"defaultSecret"`
 	// The type of the Service for the Ingress Controller. Valid Service types are: NodePort and LoadBalancer.
 	// +kubebuilder:validation:Enum=NodePort;LoadBalancer
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	ServiceType string `json:"serviceType"`
 	// Enables the use of NGINX Ingress Resource Definitions (VirtualServer and VirtualServerRoute). Default is true.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnableCRDs *bool `json:"enableCRDs"`
 	// Enable custom NGINX configuration snippets in VirtualServer, VirtualServerRoute and TransportServer resources.
 	// Requires enableCRDs set to true.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnableSnippets bool `json:"enableSnippets"`
 	// Enables preview policies.
 	// Requires enableCRDs set to true.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnablePreviewPolicies bool `json:"enablePreviewPolicies"`
 	// A class of the Ingress controller. The Ingress controller only processes Ingress resources that belong to its
 	// class (in other words, have the annotation “kubernetes.io/ingress.class”).
 	// Additionally, the Ingress controller processes Ingress resources that do not have that annotation,
 	// which can be disabled by setting UseIngressClassOnly to true. Default is `nginx`.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	IngressClass string `json:"ingressClass"`
 	// The service of the Ingress controller.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	Service *Service `json:"service"`
 	// Ignore Ingress resources without the “kubernetes.io/ingress.class” annotation.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	UseIngressClassOnly bool `json:"useIngressClassOnly"`
 	// Namespace to watch for Ingress resources. By default the Ingress controller watches all namespaces.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	WatchNamespace string `json:"watchNamespace"`
 	// Adds a new location to the default server. The location responds with the 200 status code for any request.
 	// Useful for external health-checking of the Ingress controller.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	HealthStatus *HealthStatus `json:"healthStatus,omitempty"`
 	// Enable debugging for NGINX. Uses the nginx-debug binary. Requires ‘error-log-level: debug’ in the ConfigMapData.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	NginxDebug bool `json:"nginxDebug"`
 	// Log level for V logs.
 	// Format is 0 - 3
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:Minimum=0
 	// +kubebuilder:validation:Maximum=3
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	LogLevel uint8 `json:"logLevel"`
 	// NGINX stub_status, or the NGINX Plus API.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	NginxStatus *NginxStatus `json:"nginxStatus,omitempty"`
 	// Update the address field in the status of Ingresses resources.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	ReportIngressStatus *ReportIngressStatus `json:"reportIngressStatus,omitempty"`
 	// Enables Leader election to avoid multiple replicas of the controller reporting the status of Ingress resources
 	// – only one replica will report status.
 	// Default is true.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnableLeaderElection *bool `json:"enableLeaderElection"`
 	// A Secret with a TLS certificate and key for TLS termination of every Ingress host for which TLS termination is enabled but the Secret is not specified.
 	// The secret must be of the type kubernetes.io/tls.
@@ -104,44 +124,54 @@ type NginxIngressControllerSpec struct {
 	// If the argument is set, but the Ingress controller is not able to fetch the Secret from Kubernetes API, the Ingress Controller will fail to start.
 	// Format is namespace/name.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	WildcardTLS string `json:"wildcardTLS"`
 	// NGINX or NGINX Plus metrics in the Prometheus format.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	Prometheus *Prometheus `json:"prometheus,omitempty"`
 	// Bucketed response times from when NGINX establishes a connection to an upstream server to when the last byte of the response body is received by NGINX.
 	// **Note** The metric for the upstream isn't available until traffic is sent to the upstream.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnableLatencyMetrics bool `json:"enableLatencyMetrics"`
 	// Initial values of the Ingress Controller ConfigMap.
 	// Check https://docs.nginx.com/nginx-ingress-controller/configuration/global-configuration/configmap-resource/ for
 	// more information about possible values.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	ConfigMapData map[string]string `json:"configMapData,omitempty"`
 	// The GlobalConfiguration resource for global configuration of the Ingress Controller.
 	// Format is namespace/name.
 	// Requires enableCRDs set to true.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	GlobalConfiguration string `json:"globalConfiguration"`
 	// Enable TLS Passthrough on port 443.
 	// Requires enableCRDs set to true.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	EnableTLSPassthrough bool `json:"enableTLSPassthrough"`
 	// App Protect support configuration.
 	// Requires enableCRDs set to true.
 	// +kubebuilder:validation:Optional
 	// +nullable
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	AppProtect *AppProtect `json:"appProtect"`
 	// Timeout in milliseconds which the Ingress Controller will wait for a successful NGINX reload after a change or at the initial start.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	NginxReloadTimeout int `json:"nginxReloadTimeout"`
 }
 
 // NginxIngressControllerStatus defines the observed state of NginxIngressController
 type NginxIngressControllerStatus struct {
 	// Deployed is true if the Operator has finished the deployment of the NginxIngressController.
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	// +operator-sdk:csv:customresourcedefinitions:type=status
 	Deployed bool `json:"deployed"`
 }
 
@@ -218,6 +248,7 @@ type ReportIngressStatus struct {
 	// Note: If serviceType is LoadBalancer or reportIngressStatus.externalService is set, the value of this field
 	// will be ignored.
 	// +kubebuilder:validation:Optional
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
 	IngressLink string `json:"ingressLink,omitempty"`
 }
 
